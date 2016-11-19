c5bd77dba9bb1488cf28ee8daa0256bbbbe461bd
Aori Nevo <aori.nevo@bloomingdales.com>
2016-11-19 14:46:55 -0500

    Initial commit with test index.html file.

---
+++ hbs-components/CreditCardActionModel.js
@@ -0,0 +1,177 @@
+//* ========================
+//#FILE - CreditCardActionModel.js
+//> Author: Aori Nevo
+//>
+//> Create Date: <October 24, 2016>
+//>
+//======================== */
+
+define( [
+    'backbone',    
+    'underscore',
+    'jquery'
+], function ( Backbone, _, $ ) {
+    // Private variables
+    // -----------------
+    // `dataParamsDefault` is set to a non-empty object so that postBuilder.js makes a an ajax post request.  
+    //  If the intended ajax call is a GET request, dataParams property needs to be set to an empty object.
+    // `quickToolsLinks` is the default object that the model uses to generate the QuickTools links.
+    // `dataAction, dataParams,` and ` dataCoremetrics` correspond to `data-action, data-params,` and `data-coremetrics`.
+    // `column` can be set to `left` or `right`. Note: If set to anything else, then the result will be equivalent to setting showLink to false.
+    // `showLink` is used to suppress a link from being rendered to the DOM. Note: If `showLink` is set to true and `column` 
+    /// property is not set to "left" or "right", then link will not display.
+    var props,
+        dataCoremetricsPrefix = "creditcardlanding_credit_gateway-_-bloomingdales_credit-_-",
+        qti18nPrefix = "quickTools.",
+        dataParamsDefault = '{&quot;CID&quot; : &quot;&quot;}',
+        quickToolsLinks = {
+            dispute: {
+                dataAction: "dispute",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "dispute_a_transaction",
+                qti18n: qti18nPrefix + "dispute",
+                column: "left",
+                showLink: true
+            },
+            replaceCard: {
+                dataAction: "replaceCard",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "order_a_replacement_card",
+                qti18n: qti18nPrefix + "order",
+                column: "left",
+                showLink: true
+            },
+            updateBilling: {
+                dataAction: "updateBilling",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "update_my_billing_info",
+                qti18n: qti18nPrefix + "updateBillInfo",
+                column: "left",
+                showLink: true
+            },
+            addAuthorizedUser: {
+                dataAction: "addAuthorizedUser",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "add_an_authorized_user",
+                qti18n: qti18nPrefix + "addAuthorizedUser",
+                column: "left",
+                showLink: false
+            },
+            lostCard: {
+                dataAction: "lostCard",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "report_a_lost_or_stolen_card",
+                qti18n: qti18nPrefix + "report",
+                column: "right",
+                showLink: true
+            },
+            creditAlerts: {
+                dataAction: "creditAlerts",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "manage_my_credit_alerts",
+                qti18n: qti18nPrefix + "manageAlerts",
+                column: "right",
+                showLink: true
+            },
+            estatements: {
+                dataAction: "estatements",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "manage_paperless_settings",
+                qti18n: qti18nPrefix + "managePaperless",
+                column: "right",
+                showLink: true
+            },
+            increaseLimit: {
+                dataAction: "increaseLimit",
+                dataParams: dataParamsDefault,
+                dataCoremetrics: dataCoremetricsPrefix + "request_a_credit_limit_increase",
+                qti18n: qti18nPrefix + "increaseLimit",
+                column: "right",
+                showLink: false
+            }
+        };
+
+    // Private Functions
+    // ---------------------  
+
+    // Keys `quickToolsLinks` with `dataAction` and sets `property` to `value`.
+    // @param {string} dataAction
+    // @param {string} property
+    // @param {dynamic} value
+    //
+    function setQuickToolsLinkProperty( dataAction, property, value ) {
+        quickToolsLinks[ dataAction ][ property ] = value;
+    }
+
+    // Iterates over `quickToolsLinks` object and sets `property` to `value`.
+    // This function differes from `setQuickToolsLinkProperty` in that it sets the same property in each element of `quickToolsLinks`
+    // to `value`.
+    // @param {string} property
+    // @param {dynamic} value
+    //
+    function setPropertyForAllQuickToolsLinks( property, value ) {
+        for ( var link in quickToolsLinks ) {
+            quickToolsLinks[ link ][ property ] = value;
+        }
+    }
+
+    // Returns elements of `quickToolsLinks` for which `property === value` and `showLink === true`
+    // @param {string} property
+    // @param {dynamic} value
+    // @return {array}
+    //
+    function getQuickToolsLinks( property, value ) {
+        return _.pick( quickToolsLinks, function ( link, key, object ) {
+            return link[ property ] === value && link[ "showLink" ] === true;
+        } );
+    }
+
+    // Toggle the visibility of a list of `linkKeys`.  Note, `visible` is an array that must have the same length as `linkKeys`.
+    // @param {array} linkKeys
+    // @param {array} visible
+    //
+    function setVisibilityOfQuickToolsLinks( linkKeys, visible ) {
+        linkKeys.forEach( function ( link, index ) {
+            setQuickToolsLinkProperty( link, "showLink", visible[ index ] );
+        } );
+    }
+
+    // CreditCardActionModel
+    // ---------------------        
+    var CreditCardActionModel = Backbone.Model.extend( {
+        initialize: function ( options ) {
+            props.secureHost = "";
+            this.updateQuickToolsLinks( this.get( 'ccData' ) );
+            this.set( {
+                quickToolsLinks: quickToolsLinks,
+                props: props,
+                walletURL: props.secureHost + "/account/wallet?ocwallet=true&cm_sp=creditcardlanding_credit_gateway-_-bloomingdales_credit-_-view_bwallet"
+            } );
+        },
+        // `updateQuickToolsLinks` takes credit card data and updates the `quickToolsLinks` object and sets the quickToolsLinksLeft and quickToolsLinksRight 
+        // model attributes.
+        updateQuickToolsLinks: function ( ccData ) {
+            var dataActions = [];
+            if ( ccData.cardId ) {
+                setPropertyForAllQuickToolsLinks( "dataParams", "{&quot;CID&quot;: &quot;" + ccData.cardId + "&quot;}" );
+            }
+            if ( props.creditAuthUserEnabled ) {
+                dataActions = [ "addAuthorizedUser", "lostCard", "increaseLimit" ];
+                if ( ccData.authorizedUser ) {
+                    setVisibilityOfQuickToolsLinks( dataActions, [ false, false, false ] );
+                } else if ( ccData.primaryUser || ccData.secondaryUser ) {
+                    setVisibilityOfQuickToolsLinks( dataActions, [ true, true, true ] );
+                }
+            }
+            this.set(
+                $.extend( {}, ccData, {
+                    ccData: ccData,
+                    quickToolsLinksLeft: getQuickToolsLinks( "column", "left" ),
+                    quickToolsLinksRight: getQuickToolsLinks( "column", "right" )
+                } )
+            );
+        }
+    } );
+
+    return CreditCardActionModel;
+} );
