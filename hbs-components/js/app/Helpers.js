ad914608217dca140629729126b050fe63ac5b95
Aori Nevo <aori.nevo@bloomingdales.com>
2016-11-19 22:39:35 -0500

    Last fix for loading hbs files

--- hbs-components/js/app/Helpers.js
+++ hbs-components/js/app/Helpers.js
@@ -1,4 +1,4 @@
-define(['handlebars', 'text!/templates/QuickToolsLinks.hbs'],
+define(['handlebars', 'text!templates/QuickToolsLinks.hbs'],
 function( Handlebars, QuickToolsLinks){
   Handlebars.registerPartial('quickToolsLinks', Handlebars.compile( QuickToolsLinks ));
 });
