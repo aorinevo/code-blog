13a57e5c78caf11b352803a04d18299bf24b59b6
Aori Nevo <aori.nevo@bloomingdales.com>
2016-11-28 18:47:55 -0500

    Add multiple meters to svg-meter-example

--- code-blog/src/app/svg-meter-example/svg-meter-example.component.ts
+++ code-blog/src/app/svg-meter-example/svg-meter-example.component.ts
@@ -5,8 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './svg-meter-example.component.html',
   styleUrls: ['./svg-meter-example.component.css']
 })
-export class SvgMeterExampleComponent implements OnInit {      
-  
+export class SvgMeterExampleComponent implements OnInit {       
+  meters: Object[] =[
+    {
+      radiusOuter: 100,
+      radiusInner: 80,
+      part: 3000,
+      whole: 5000,
+      strokeWhole: 'gray',
+      strokePart: 'red',
+      strokeWidth: 20
+    },
+    {
+      radiusOuter: 100,
+      radiusInner: 95,
+      part: 2000,
+      whole: 5000,
+      strokeWhole: 'gray',
+      strokePart: 'blue',
+      strokeWidth: 5
+    },
+    {
+      radiusOuter: 100,
+      radiusInner: 90,
+      part: 4000,
+      whole: 5000,
+      strokeWhole: 'gray',
+      strokePart: 'green',
+      strokeWidth: 10
+    }
+  ];
   
   constructor() { 
   }
