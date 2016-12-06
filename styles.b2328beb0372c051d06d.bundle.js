5faa8247bde0c4139eb503aff557b114a91eebe7
Aori Nevo <aori.nevo@bloomingdales.com>
2016-12-05 22:37:41 -0500

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{368:function(n,o){},695:function(n,o,c){n.exports=c(368)}},[695]);
+webpackJsonp([1,2],{368:function(n,o){},698:function(n,o,c){n.exports=c(368)}},[698]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
